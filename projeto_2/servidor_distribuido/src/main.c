5509965545c8ef9f781f317fdb59fb5bf602e34e
romulosouza <romulovinicius10@gmail.com>
2021-04-21 16:31:38 -0300

    Adicionado serializer para temperatura

--- projeto_2/servidor_distribuido/src/main.c
+++ projeto_2/servidor_distribuido/src/main.c
@@ -3,24 +3,35 @@
 #include <sys/types.h>
 #include <signal.h>
 
-#include <csv.h>
 #include <gpio.h>
 #include <bme280.h>
 #include <i2c_bme.h>
 
 #include <server.h>
 #include <client.h>
-#include <pthread.h>
+#include <serializer.h>
 
-// pthread_t server_thread;
+struct identifier id;
+struct bme280_dev dev;
 
-void sig_handler(int signal){
+void sig_alarm(int signal){
+    float temperature, humidity;
+
+    read_temperature_i2c(&dev, &temperature, &humidity);
+    printf("Evironment temperature = %f\n", temperature);
+    printf("Humidity = %f\n", humidity);
+
+    char serialized_temperature[MAX_BUFFER_SIZE];
+    serialize_temperature(serialized_temperature, temperature, humidity);
+    send_message(serialized_temperature);
+
+    alarm(1);
+}
+
+void sig_stop(int signal){
     if (signal == SIGINT){
-        // close_file();
-
         printf("Finalizando as threads...\n");
         stop_server();
-        // pthread_cancel(server_thread);
         close_client_socket();
         sleep(1);
 
@@ -29,41 +40,25 @@ void sig_handler(int signal){
 }
 
 int main(int argc, const char * argv[]) {
-    // float te, humidity;
 
-    struct identifier id;
-    struct bme280_dev dev;
-
-    signal(SIGINT, sig_handler);
+    signal(SIGINT, sig_stop);
+    signal(SIGALRM, sig_alarm);
 
     // BME configuration
     setup_i2c_bme(&id, &dev);
     open_i2c_conn(&id, &dev);
     initialize_I2C(&dev);
 
+    // Schedule alarm to read temperature
+    alarm(1);
+
     // GPIO configuration
     setup_gpio(&states);
-
-    // Start server socket
-    // pthread_create(&server_thread, NULL, (void*)create_server, NULL);
 
     // Start client socket
     open_client_socket();
 
-    // char msg[] = {"minha primeira msg de teste\n"};
-    // send_message(msg);
-
     create_server();
-
-    // while(1){
-    //     read_temperature_i2c(&dev, &te, &humidity);
-    //     printf("Evironment temperature = %f\n", te);
-    //     printf("Humidity = %f\n", humidity);
-
-    //     // toggle_switch(0);
-
-    //     sleep(2);
-    // }
 
    return 0;
 }
--- projeto_2/servidor_distribuido/src/serializer.c
+++ projeto_2/servidor_distribuido/src/serializer.c
@@ -9,3 +9,34 @@ void serialize(char *buff, int *size, char key[], int value, char flag[]){
 
     buff[*size] = '\0';
 }
+
+void serialize_temperature(char *buff, float temperature, float humidity){
+    char flag[] = "%d.%04d";
+    int size = 0;
+
+    char str_temperature[] = "temperature:";
+    memcpy(buff + size, (const void*) str_temperature, strlen(str_temperature));
+    size += strlen(str_temperature);
+
+    int int_temperature = temperature;
+    float dec = temperature - int_temperature;
+    int int_dec_temperature = dec * 10000;
+    sprintf(&buff[size], flag, int_temperature, int_dec_temperature);
+    size += 7; // <2_digit_number>.<4_digit_decimal>
+
+    buff[size++] = ';';
+
+    char str_humidity[] = "humidity:";
+    memcpy(buff + size, (const void*) str_humidity, strlen(str_humidity));
+    size += strlen(str_humidity);
+
+    int int_humidity = humidity;
+    dec = humidity - int_humidity;
+    int int_dec_humidity = dec * 10000;
+    sprintf(&buff[size], flag, int_humidity, int_dec_humidity);
+    size += 7; // <number>.<4_digit_decimal>
+
+    buff[size++] = ';';
+
+    buff[size] = '\0';
+}
